ed9c53fc13dc61db93ebbc62b8847f7fe8b4e812
juliayang18 <juliayang18@gmail.com>
2024-08-19 13:02:09 -0400

    Adding new files

---
+++ input.py
@@ -0,0 +1,10 @@
+# input 取得使用者輸入
+name = input("請輸入名字: ")
+print (f"你的名字是 {name}")
+
+# practice calculation
+length = float (input ("Please enter the length: "))
+width = float (input ("Please enter the width: "))
+
+area = length * width
+print (f"The area is {area}")
